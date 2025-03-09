4d21f00e738a41334fd5f92475be91e9fe051e44
subitbhattarai <subitbhattarai@gmail.com>
2025-03-09 23:25:49 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const title = 'Basic List';
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: title,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Hello World App')),
-        body: const Center(
-          child: Text(
-            'Hello, World!',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
+        appBar: AppBar(title: const Text(title)),
+        body: ListView(
+          children: const <Widget>[
+            ListTile(leading: Icon(Icons.map), title: Text('Map')),
+            ListTile(leading: Icon(Icons.photo_album), title: Text('Album')),
+            ListTile(leading: Icon(Icons.phone), title: Text('Phone')),
+          ],
         ),
       ),
     );
